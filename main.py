b8479714bfb5350ead86ae5e19b744b7b319a89c
Keiane Balicanta <74569357+keiane@users.noreply.github.com>
2023-11-03 02:17:15 -0400

    Darkened text

--- main.py
+++ main.py
@@ -32,11 +32,16 @@ import gradio as gr
 # CSS theme styling
 theme = gr.themes.Base(
     font=[gr.themes.GoogleFont('Montserrat'), 'ui-sans-serif', 'system-ui', 'sans-serif'],
+    primary_hue="emerald",
+    secondary_hue="emerald",
+    neutral_hue="zinc"
 ).set(
-    body_text_color='black',
-    body_text_color_dark='black',
-    body_text_color_subdued='*neutral_800',
-    body_text_color_subdued_dark='*neutral_800'
+    body_text_color='*neutral_950',
+    body_text_color_subdued='*neutral_950',
+    block_shadow='*shadow_drop_lg',
+    button_shadow='*shadow_drop_lg',
+    block_title_text_color='*neutral_950',
+    block_title_text_weight='500'
 )
 
 ### MAIN FUNCTION
